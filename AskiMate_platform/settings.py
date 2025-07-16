1fe093063a6317bd4ce66cd36ce0d32c0ba7d009
Vahid Mohammadi <vahidmoir@gmail.com>
2025-07-16 13:46:44 +0330

    Update settings.py

--- AskiMate_platform/settings.py
+++ AskiMate_platform/settings.py
@@ -29,7 +29,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 ROOT_URLCONF = 'AskiMate_platform.urls'
